c73aa527e984a1f352ed9c166547dfb9a4ab4def
Smit2784 <Smitdudhat2507@gmail.com>
2025-11-03 19:29:46 +0530

    Fix: remove non-existent Header import from HomePage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Header from '../common/Layout/Header';
 import Hero from '../components/home/Hero';
 import MovieGrid from '../components/movies/MovieGrid';
 
-export default function HomePage(){
+export default function HomePage() {
   return (
     <div className="space-y-10">
       <Hero />
